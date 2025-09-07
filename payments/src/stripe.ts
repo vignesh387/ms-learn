45817b30e4259220fb37153633254f7129698298
vignesh387 <vigneshyyy5@gmail.com>
2025-09-07 11:03:27 +0400

    test stripe key changes

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from "stripe";
 
 process.env.TEST_DATA = "hnfrAm8rOkryFEnV23jjfFlw";
 
-export const stripe = new Stripe(process.env.TEST_DATA!, {
+export const stripe = new Stripe("sk_test_hnfrAm8rOkryFEnV23jjfFlw", {
   apiVersion: "2024-09-30.acacia",
 });
